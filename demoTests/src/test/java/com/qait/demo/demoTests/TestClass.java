34fca35822f63178f3b4288dd07f347b17a15857
Dhruv Pande <46739476+dhruvpande-qait@users.noreply.github.com>
2019-12-31 12:45:35 +0530

    Update TestClass.java

--- demoTests/src/test/java/com/qait/demo/demoTests/TestClass.java
+++ demoTests/src/test/java/com/qait/demo/demoTests/TestClass.java
@@ -7,11 +7,11 @@ import junit.framework.Assert;
 public class TestClass {
 	
 	@Test
-	public void test_1(){
+	public void step01(){
 		Assert.assertEquals("hello", "hello");
 	}
 	@Test
-	public void test_1(){
+	public void step02(){
 		Assert.assertEquals("hello", "hellqqqq");
 	}
 }
